3721021d96a2649924c98ee2656b52b942732c56
Huy Hậu ( hojuz) <115852407+Vohuyhau@users.noreply.github.com>
2022-10-22 17:29:52 +0700

    a

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
     <link rel="stylesheet" href="css/owl.carousel.min.css" type="text/css">
     <link rel="stylesheet" href="css/slicknav.min.css" type="text/css">
     <link rel="stylesheet" href="css/style.css" type="text/css">
-    <link rel="shortcut icon" href="ATNtoy\Logo.png">
+    <link rel="shortcut icon" href="ATNtoy\ATNlogo.png">
 </head>
 
 <body>
