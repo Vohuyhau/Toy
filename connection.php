f737ec3e97e5de011a5194e952e50fe6c83aeea8
Huy Hậu ( hojuz) <115852407+Vohuyhau@users.noreply.github.com>
2022-10-25 16:29:15 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-$conn = pg_connect("postgres://eqsswfxaphixgx:79c5717c086ecfdb796105255f2a0f48e0ec8b5c2278c2e680b24bb8689abc65@ec2-54-145-139-208.compute-1.amazonaws.com:5432/d200jeu77acrei");
+$conn = pg_connect("postgres://kupukrqgfasefl:67dc47cfdf8e72ab50986f21e1954a31156356bc2a4f5635060adb40c43adcfa@ec2-52-86-115-245.compute-1.amazonaws.com:5432/dttrarmo6kits");
 	echo 'Connected Successfully!!!';
 	if(!$conn)
 	{
